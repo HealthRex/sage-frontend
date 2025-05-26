fa762b6ec811bba564b22269524badd0a2b9fe57
Nodir <y_nodir@yahoo.com>
2025-05-26 19:53:29 +0200

    Revert "Trying to make POC work with a private service - maybe it helps with CORS."
    This reverts commit d4266f8d5331347d5a766ea13df7e6072f3905d2.

--- src/app/poc/const.tsx
+++ src/app/poc/const.tsx
@@ -1,3 +1,2 @@
-export const BACKEND = 'assist-pc-backend-ly25:10000'
-// export const BACKEND = 'https://assist-pc-backend-dev.onrender.com'
+export const BACKEND = 'https://assist-pc-backend-dev.onrender.com'
 // export const BACKEND = 'http://localhost:3000'
