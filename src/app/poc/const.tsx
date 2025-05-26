c6c6166904c02919a2a190cd77d6f96914fe68ce
Nodir <y_nodir@yahoo.com>
2025-05-26 19:05:05 +0200

    The protocol looks like is an HTTP.

--- src/app/poc/const.tsx
+++ src/app/poc/const.tsx
@@ -1,3 +1,3 @@
-export const BACKEND = 'https://assist-pc-backend-ly25:10000'
+export const BACKEND = 'http://assist-pc-backend-ly25:10000'
 // export const BACKEND = 'https://assist-pc-backend-dev.onrender.com'
 // export const BACKEND = 'http://localhost:3000'
