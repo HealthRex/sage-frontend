22345d7c4578311c7f6efda611488d5e9c565a4c
Kanav Chopra <72208392+code-kanav@users.noreply.github.com>
2025-07-02 14:38:27 -0700

    updated backend url

--- src/components/multiStepPageComponent.tsx
+++ src/components/multiStepPageComponent.tsx
@@ -44,7 +44,7 @@ export default function MultiStepPageComponent() {
     setLoading(true)
   
     try {
-      const response = await fetch(process.env.BACKEND_URL + '/referral-streamed', {
+      const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/referral-streamed', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
